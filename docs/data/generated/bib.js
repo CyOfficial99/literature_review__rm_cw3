62335c69ffa3c688b8130372b14283c51842e76f
Chin Chee Yuan <87355118+CyOfficial99@users.noreply.github.com>
2026-05-02 19:29:11 +0100

    Add files via upload

---
+++ docs/data/generated/bib.js
@@ -0,0 +1,101 @@
+﻿define({ entries : {
+    "an2024vitality2reviewingacademic": {
+        "archiveprefix": "arXiv",
+        "author": "Hongye An and Arpit Narechania and Emily Wall and Kai Xu",
+        "eprint": "2408.13450",
+        "primaryclass": "cs.HC",
+        "title": "vitaLITy 2: Reviewing Academic Literature Using Large Language Models",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2408.13450",
+        "year": "2024"
+    },
+    "chou2011": {
+        "author": "Chou, Jeng-Hong and Yang, Chuan\u2010Kai",
+        "doi": "10.1111/j.1467-8659.2011.01921.x",
+        "journal": "Computer Graphics Forum",
+        "month": "jun,",
+        "number": "3",
+        "pages": "721-730",
+        "title": "PaperVis: Literature Review Made Easy",
+        "type": "article",
+        "url": "https://doi.org/10.1111/j.1467-8659.2011.01921.x",
+        "volume": "30",
+        "year": "2011"
+    },
+    "delgadochaves2025": {
+        "author": "Delgado-Chaves, Fernando M. and Jennings, Matthew J. and Atalaia, Ant\u00f3nio and Wolff, Justus and Horv\u00e1th, Rita and Mamdouh, Zeinab M. and Baumbach, Jan and Baumbach, Linda",
+        "doi": "10.1073/pnas.2411962122",
+        "journal": "Proceedings of the National Academy of Sciences",
+        "month": "jan,",
+        "number": "2",
+        "pages": "e2411962122",
+        "title": "Transforming literature screening: The emerging role of large language models in systematic reviews",
+        "type": "article",
+        "url": "https://www.pnas.org/doi/pdf/10.1073/pnas.2411962122",
+        "volume": "122",
+        "year": "2025"
+    },
+    "elarini2011": {
+        "author": "El-Arini, Khalid and Guestrin, Carlos",
+        "doi": "10.1145/2020408.2020479",
+        "month": "aug,",
+        "pages": "439-447",
+        "title": "Beyond keyword search",
+        "type": "article",
+        "url": "https://doi.org/10.1145/2020408.2020479",
+        "year": "2011"
+    },
+    "farquhar2024": {
+        "author": "Farquhar, Sebastian and Kossen, Jannik and Kuhn, Lorenz and Gal, Yarin",
+        "doi": "10.1038/s41586-024-07421-0",
+        "journal": "Nature",
+        "month": "jun,",
+        "number": "8017",
+        "pages": "625-630",
+        "title": "Detecting hallucinations in large language models using semantic entropy",
+        "type": "article",
+        "url": "https://www.nature.com/articles/s41586-024-07421-0.pdf",
+        "volume": "630",
+        "year": "2024"
+    },
+    "lewis2021retrievalaugmentedgenerationknowledgeintensivenlp": {
+        "archiveprefix": "arXiv",
+        "author": "Patrick Lewis and Ethan Perez and Aleksandra Piktus and Fabio Petroni and Vladimir Karpukhin and Naman Goyal and Heinrich K\u00fcttler and Mike Lewis and Wen-tau Yih and Tim Rockt\u00e4schel and Sebastian Riedel and Douwe Kiela",
+        "eprint": "2005.11401",
+        "primaryclass": "cs.CL",
+        "title": "Retrieval-Augmented Generation for Knowledge-Intensive NLP Tasks",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2005.11401",
+        "year": "2021"
+    },
+    "wang2024autosurveylargelanguagemodels": {
+        "archiveprefix": "arXiv",
+        "author": "Yidong Wang and Qi Guo and Wenjin Yao and Hongbo Zhang and Xin Zhang and Zhen Wu and Meishan Zhang and Xinyu Dai and Min Zhang and Qingsong Wen and Wei Ye and Shikun Zhang and Yue Zhang",
+        "eprint": "2406.10252",
+        "primaryclass": "cs.IR",
+        "title": "AutoSurvey: Large Language Models Can Automatically Write Surveys",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2406.10252",
+        "year": "2024"
+    },
+    "xie2026awesomelithypothesisgenerationagentsupported": {
+        "archiveprefix": "arXiv",
+        "author": "Zefei Xie and Yuhan Guo and Kai Xu",
+        "eprint": "2603.22648",
+        "primaryclass": "cs.HC",
+        "title": "AwesomeLit: Towards Hypothesis Generation with Agent-Supported Literature Research",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2603.22648",
+        "year": "2026"
+    },
+    "yao2024llmlieshallucinationsbugs": {
+        "archiveprefix": "arXiv",
+        "author": "Jia-Yu Yao and Kun-Peng Ning and Zhen-Hui Liu and Mu-Nan Ning and Yu-Yang Liu and Li Yuan",
+        "eprint": "2310.01469",
+        "primaryclass": "cs.CL",
+        "title": "LLM Lies: Hallucinations are not Bugs, but Features as Adversarial Examples",
+        "type": "misc",
+        "url": "https://arxiv.org/abs/2310.01469",
+        "year": "2024"
+    }
+}});
